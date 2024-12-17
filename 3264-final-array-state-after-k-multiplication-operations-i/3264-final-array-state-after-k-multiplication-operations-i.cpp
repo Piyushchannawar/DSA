9fdf8c2a6ccc13974fa90bd3fa3d53f64602389e
Piyushchannawar <133411070+Piyushchannawar@users.noreply.github.com>
2024-12-17 22:38:07 +0530

    Time: 3 ms (30.87%), Space: 27.1 MB (46.57%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        while(k){
-           int min = nums[0];
-           int i = 0;
-           for(int j=0;j<nums.size();j++){
-            if(nums[j]<min){
-                i=j;
-            }
-           }
-           nums[i] = nums[i] * multiplier;
-           k--;
+        for(int i = 0; i < k; ++i) {
+            auto minIt = min_element(nums.begin(), nums.end());
+            *minIt *= multiplier;
         }
         return nums;
     }
